770fd3f14207364176c96097ac416e01580104b5
Issifou88 <94397775+Issifou88@users.noreply.github.com>
2021-11-16 22:02:03 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include "exercice1_serie1.h"
+#include "exercice2_serie1.h"
+#include "exercice3_serie1.h"
+#include "exercice4_serie1.h"
+#include "exercice5_serie1.h"
+#include "exercice6_serie1.h"
+#include "exercice7_serie1.h"
+#include "exercice8_serie1.h"
+void main ()
+{
+    printf("ex1.s1:\n\n");
+    exercice1();
+    printf("\n");
+    printf("ex2.s1:\n\n");
+    exercice2();
+    printf("\n");
+    printf("ex3.s1:\n\n");
+    exercice3();
+    printf("\n");
+    printf("ex4.s1:\n\n");
+    exercice4();
+    printf("\n");
+    printf("ex5.s1:\n\n");
+    exercice5();
+    printf("\n");
+    printf("ex6.s1:\n\n");
+    exercice6();
+    printf("\n");
+    printf("ex7.s1:\n\n");
+    exercice7();
+    printf("\n");
+    printf("ex8.s1:\n");
+    exercice8();
+    printf("\n");
+}
